896bb5680ef9455a6185744c6a6a50340334ae07
Sarith-Samarakoon <IT22207036@my.sliit.lk>
2024-12-11 00:19:48 +0530

    Resume page created
    CSS, JS and Images are included

---
+++ JS/Resume.js
@@ -0,0 +1,19 @@
+// Get the button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+scrollToTopBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Smooth scroll
+  });
+};
